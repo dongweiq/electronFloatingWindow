33575723463b56d4a75793fe8e151c3cfe076efb
王洪贺 <dongweiqmail@gmail.com>
2025-05-20 17:47:27 +0800

    fix: 添加打开悬浮窗快捷键

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
 require('@electron/remote/main').initialize();
 
+let mainWindow = null;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 380,
     height: 380,
     transparent: true,
@@ -11,6 +13,7 @@ function createWindow() {
     alwaysOnTop: true,
     resizable: false,
     maximizable: false,
+    skipTaskbar: true, // 不在任务栏显示
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -18,14 +21,42 @@ function createWindow() {
     }
   });
 
-  win.setMovable(true);
+  // 设置窗口层级为浮动层，确保在最上层
+  mainWindow.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+  mainWindow.setAlwaysOnTop(true, 'screen-saver'); // 设置为最高层级
 
-  require('@electron/remote/main').enable(win.webContents);
-  win.loadFile('index.html');
+  // 允许拖动
+  mainWindow.setMovable(true);
+
+  require('@electron/remote/main').enable(mainWindow.webContents);
+  mainWindow.loadFile('index.html');
+
+  // 初始隐藏窗口
+  mainWindow.hide();
+}
+
+function toggleWindow() {
+  if (!mainWindow) {
+    createWindow();
+    return;
+  }
+
+  if (mainWindow.isVisible()) {
+    mainWindow.hide();
+  } else {
+    mainWindow.show();
+    mainWindow.setAlwaysOnTop(true, 'screen-saver');
+    mainWindow.focus();
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
+
+  // 注册全局快捷键
+  globalShortcut.register('Alt+Space', () => {
+    toggleWindow();
+  });
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -38,4 +69,9 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
+});
+
+// 注销快捷键
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
 }); 
